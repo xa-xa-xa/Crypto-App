0280d03643a11fc301850844dafb6d78654bdf97
Alex K <askrasn@gmail.com>
2021-03-23 17:17:01 -0700

    add and use axios

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import geckoApi from "./api/geckoApi";
+
 import CoinsList from "../src/components/CoinsList/Coinlist";
 import Layout from "../src/components/Layout/Layout";
 import SearchBar from "../src/components/SearchBar";
@@ -24,10 +26,26 @@ const Home = ({ fetchedData }) => {
 export default Home;
 
 export const getServerSideProps = async () => {
-  const res = await fetch(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false"
-  );
-  const fetchedData = await res.json();
+  // const res = await fetch(
+  //   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false"
+  // );
+
+  // const fetchedData = await res.json();
+
+  //* Axios variant:
+  const res = await geckoApi.get("/markets", {
+    params: {
+      vs_currency: "usd",
+      order: "market_cap_desc",
+      per_page: "20",
+      page: "1",
+      sparkline: "false"
+    }
+  });
+
+  const fetchedData = await res.data;
+
+  console.log("------fetchedData-----------------",  fetchedData.data);
 
   if (!fetchedData) return { notFound: true };
   return { props: { fetchedData } };
